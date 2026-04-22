6ea2dd0a354514d773bb3f9181dc4c19f4fa0ae2
Shreyasinha518 <shreyasinhamisti999@gmail.com>
2026-04-23 00:11:29 +0530

    Adding dsa practice

---
+++ PATTERN PRINTING/Star.java
@@ -0,0 +1,23 @@
+import java .util.Scanner;
+public class Star {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n;
+        System.out.println("Enter the number:");
+        n=in.nextInt();
+         int mid=(n/2)+1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+               
+                if(i==mid||j==mid)
+                    System.out.print("* ");
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
+
+    }
+  
+    
+    
+}
