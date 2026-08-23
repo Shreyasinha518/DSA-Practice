2192443763361707688bf24a41784d5bd04f71c8
Shreyasinha518 <shreyasinhamisti999@gmail.com>
2026-08-23 16:57:51 +0530

    Adding dsa practice

---
+++ OOPS/Constructors/studentData.java
@@ -0,0 +1,56 @@
+//package Constructors;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class studentData {
+
+    public static class Student {
+        String name;
+        int Rno;
+        double[] marks;
+
+        // Default constructor
+        Student() {
+        }
+
+        // Constructor with array size
+        Student(int s, Scanner in) {
+            marks = new double[s];
+
+            System.out.println("Enter the array values:");
+
+            for (int i = 0; i < s; i++) {
+                marks[i] = in.nextDouble();
+            }
+        }
+
+        // Constructor with array
+        Student(double[] s) {
+            marks = Arrays.copyOf(s, s.length); // Deep copy
+        }
+
+        void print() {
+            System.out.println("Name : " + name);
+            System.out.println("Roll no : " + Rno);
+            System.out.println("Array of Marks:");
+
+            for (int i = 0; i < marks.length; i++) {
+                System.out.print(marks[i] + " , ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+
+        Student s = new Student(n, in);
+        s.name = "Shreya";
+        s.Rno = 120;
+
+        s.print();
+
+        in.close();
+    }
+}
