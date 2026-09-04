5ae7d06eacd9590c7bab789d84f613767e0e6da9
Shreyasinha518 <shreyasinhamisti999@gmail.com>
2026-09-05 00:56:25 +0530

    Adding dsa practice

---
+++ OOPS/QUESTIONS/complexNumber.java
@@ -0,0 +1,66 @@
+//package OOPS.QUESTIONS;
+import java.util.*;
+class complex{
+    int r;
+    int i;
+    complex(int r,int i){
+        this.r=r;
+        this.i=i;
+    }
+    void print(){
+        if (i<0){
+            System.out.println("Complex number: "+r+" - "+(-i)+"i");
+        }
+        else{
+
+        
+        System.out.println("Complex number: "+r+" + "+i+"i");
+    }
+}
+void add(complex z){
+    int real=this.r+z.r;
+    int imag=this.i+z.i;
+    if (imag<0){
+        System.out.println("Sum of complex numbers: "+real+" - "+(-imag)+"i");
+    }
+    else{
+    System.out.println("Sum of complex numbers: "+real+" + "+imag+"i");
+}
+}
+void subtract(complex z){
+    int real=this.r-z.r;
+    int imag=this.i-z.i;
+    if (imag<0){
+        System.out.println("Difference of complex numbers: "+real+" - "+(-imag)+"i");
+    }
+    else{
+    System.out.println("Difference of complex numbers: "+real+" + "+imag+"i");
+}
+}
+void multiply(complex z){
+    int real=this.r*z.r-this.i*z.i;
+    int imag=this.r*z.i+this.i*z.r;
+    if (imag<0){
+        System.out.println("Product of complex numbers: "+real+" - "+(-imag)+"i");
+    }
+    else{
+    System.out.println("Product of complex numbers: "+real+" + "+imag+"i");
+}
+}
+}
+
+public class complexNumber {
+    public static void main(String[] args) {
+        complex z1=new complex(3,4);
+        complex z2=new complex(5,-6);
+        z1.print();
+        z2.print();
+        z1.add(z2);
+        z1.subtract(z2);
+        z1.multiply(z2);
+
+    }
+
+    
+}
+
